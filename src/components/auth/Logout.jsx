b2145450de1e35da73f13436c7e44e2dab5162e6
Ferra <chri@ferraa.it>
2022-01-04 01:59:52 +0100

    Implemented protected routes, Dashboard, Logout. Added Homepage, .env
     * Implemented Logout function
     * Added Dashboard protected route
     * Added Homepage with some links
     * Added .env file
     * Fixed some useEffect dependency warnings

---
+++ src/components/auth/Logout.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Logout() {
+  const dotenv = require("dotenv");
+  dotenv.config();
+
+  const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
+  let navigate = useNavigate();
+
+  const logout = () => {
+    axios
+      .get(BASE_API_URL + "/logout.php", { withCredentials: true })
+      .then(() => {
+        navigate(`/login`);
+      });
+  };
+
+  return <button onClick={logout}>Logout</button>;
+}
